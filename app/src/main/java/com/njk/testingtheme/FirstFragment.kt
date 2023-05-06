d4750c176d715f1008e5a7ee70da1ef3b27ec61c
nijuyonkadesu <24ksriramv@gmail.com>
2023-05-06 12:43:18 +0530

    gradle cleanup

--- app/src/main/java/com/njk/testingtheme/FirstFragment.kt
+++ app/src/main/java/com/njk/testingtheme/FirstFragment.kt
@@ -1,23 +1,11 @@
 package com.njk.testingtheme
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.installations.FirebaseInstallations
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.FirebaseMessagingService
 import com.njk.testingtheme.databinding.FragmentFirstBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 /**
@@ -25,7 +13,6 @@ import kotlinx.coroutines.launch
  */
 class FirstFragment : Fragment() {
 
-    lateinit var database: DatabaseReference
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
